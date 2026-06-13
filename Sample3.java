c6d497d58c4435771ccc112412258ab56550ff1c
AnushreeAnil <anushreeanil6@gmail.com>
2026-06-13 13:14:33 +0530

    Add files via upload

---
+++ Sample3.java
@@ -0,0 +1,15 @@
+class Sample3
+{
+public static void main(String[] args)
+{
+System.out.println(20+20);
+System.out.println("The Value is"+20);
+System.out.println("The Value is"+20+20);
+System.out.println(20+"is the value");
+System.out.println(20+20+"is the value");
+System.out.println("The Value is"+(20+20));
+System.out.println("The Value is"+(1/2));
+System.out.println("The Value is"+(1/2.0));
+
+}
+}
---
+++ one.java
@@ -0,0 +1,10 @@
+class One 
+{
+	public static void main(String[] args) 
+	{
+		for(int i=1;i<=10/2;i++)
+		{
+			System.out.println(i);
+		}
+	}
+}
